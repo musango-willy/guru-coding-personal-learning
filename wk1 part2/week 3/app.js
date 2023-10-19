321632c93b1723d8e9dd24d28aba8b9ebcff5afd
musango willy <musangowilly@gmail.com>
2023-10-19 21:46:55 +0300

    ksd

---
+++ wk1 part2/week 3/app.js
@@ -0,0 +1,28 @@
+//conditional statements
+
+//if-else statement
+
+const age=17;
+
+if(age <=18){
+
+    console.log("not eligble to vote,is underage");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//loops
