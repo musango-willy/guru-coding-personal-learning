9e117a81d53f1be5a94abb93f91143c6f10087ed
musango willy <musangowilly@gmail.com>
2023-10-19 22:00:38 +0300

    nested if statement

--- wk1 part2/week 3/app.js
+++ wk1 part2/week 3/app.js
@@ -9,11 +9,40 @@ if(age <=18){
     console.log("not eligble to vote,is underage");
 
 }else{
-    
+
     console.log("eligble to vote,overage");
 }
 
+//if-else 
 
+const  theScore=50;
+
+
+if(theScore<=39){
+
+    console.log("John failed the test,is a F9 student");
+
+}else if(theScore>=40 && theScore<=49){
+
+    console.log("John is a D student");
+
+}else if (theScore>=50 && theScore<=59){
+
+    console.log("John is a C student");
+
+}else if(theScore>=60 && theScore<=69){
+
+    console.log("John is a B student");
+
+}else if(theScore>=70){
+
+    console.log("John is a A student");
+
+}else{//negative score
+
+    console.log("no score");
+
+}
 
 
 
