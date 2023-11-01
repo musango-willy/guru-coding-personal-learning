60b4a4c90f86920cb2bd7435d54145e0c8964d19
musango willy <musangowilly@gmail.com>
2023-11-01 14:44:47 +0300

    accessing objects using dot notation

---
+++ week 6/object.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+
+const person={
+    firstname:wycliffe,
+    middlename:musango,
+    lastname:willy,
+    age:24,
+    email:"musangowilly@gmail.com",
+    hobbies:["football","music","riding"],
+
+    fillname(){
+        return'${this.firstname} ${this.lastname}'
+    }
+
+}
